756eb5582ee7a6b0ba99044198190b59ec5d6068
jo_abera <johannesab0939@gmail.com>
2025-06-03 17:13:22 +0300

    modified

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,6 +1,6 @@
-import { CardProps } from "@/interfaces";
+import { type CardProps } from "@/interfaces"; // Explicit type import
 
-const Card: React.FC<CardProps> = ({ title, content, className = "" }) => {
+const Card = ({ title, content, className = "" }: CardProps) => {
 	return (
 		<div className={`border rounded-lg p-4 shadow-sm ${className}`}>
 			<h2 className="text-xl font-bold mb-2">{title}</h2>
